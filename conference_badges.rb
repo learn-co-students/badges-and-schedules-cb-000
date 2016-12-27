a8c859c9eb152a989146ec4e9d6cb74ade4e64a6
Marcus Magnusson <marcusmagnusson@outlook.com>
2016-12-27 01:10:42 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+# should return a formatted badge
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+# should return a list of badge messages
+def batch_badge_creator(array)
+    newArray = []
+    array.each { |string| newArray.push(badge_maker(string)) }
+    newArray
+end
+
+# should return a list of welcome messages and room assignments
+def assign_rooms(array)
+    newArray = []
+    array.each_with_index { |string, index| newArray.push "Hello, #{string}! You'll be assigned to room #{index + 1}!" }
+    newArray
+end
+
+# should puts the list of badges and room_assignments
+def printer(array)
+    batch_badge_creator(array).each { |x| puts x }
+    assign_rooms(array).each { |x| puts x }
+end
