6bf2bb3fd0a83bd9d9be848e99543e2477148f08
Ni Chia <nichia@gmail.com>
2017-10-11 18:07:10 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,15 +9,15 @@ end
 def batch_badge_creator(names_list)
   badge_msgs = []
   names_list.each { |name| badge_msgs << badge_maker(name) }
-  return badge_msgs
   #binding.pry
+  return badge_msgs
 end
 
 def assign_rooms(names_list)
   room_msgs = []
   names_list.each_with_index { |name, ind| room_msgs << "Hello, #{name}! You'll be assigned to room #{ind + 1}!"}
-  return room_msgs
   #binding.pry
+  return room_msgs
 end
 
 def printer(names_list)
@@ -28,3 +28,6 @@ def printer(names_list)
   batch_badge_creator(names_list).each { |msg| puts msg }
   assign_rooms(names_list).each { |msg| puts msg }
 end
+
+#names_list=["Tony", "Terry", "Tom", "Thompson"]
+#printer(names_list)
