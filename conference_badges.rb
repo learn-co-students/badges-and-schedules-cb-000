8a2d8ba1cbea39c7d22ecb404ee520ead8c8209c
Jay Fleming <jacobmfleming@gmail.com>
2018-02-15 21:00:07 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def batch_badge_creator(array)
+  new_array = []
+  array.each |x| {
+    welcome = x + "Hello, my name is "
+    new_array.push(welcoem)
+  }
+    end
+    return new_array
+end
