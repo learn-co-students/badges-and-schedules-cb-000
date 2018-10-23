b88de56b17b9a775ad4bbb13f338eff8ffc2c1a7
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-23 04:06:30 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,5 +13,19 @@ def batch_badge_creator(arrayOfName)
 end
 
 def assign_rooms(speakerList)
+new_array = []
+speakerList.each_with_index {|name, index|
+new_array.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+}
+return new_array
+end
 
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+   puts badge
+ end
+
+ assign_rooms(attendees).each do |assignment|
+   puts assignment
+ end
 end
