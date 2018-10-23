713d33984d89097bdb111c0decce4d42bd0709a0
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-23 02:17:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arrayOfName)
+  new_array = []
+  arrayOfName.each do |name|
+    new_array.push(badge_maker(name))
+  end
+  return new_array
+end
+
+def assign_rooms(speakerList)
+
+end
