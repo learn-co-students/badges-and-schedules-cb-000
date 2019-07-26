d79d695072095b2aa8e6dcec49772e4d2a5606e0
Will Hodgson <will.hodgson@current-rms.com>
2019-07-26 09:28:14 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges << "Hello, my name is #{name}."
+  end
+  badges
+end
+
+def assign_rooms(names)
+  index = 1
+  room_list = []
+
+  names.each do |name|
+    room_list << "Hello, #{name}! You'll be assigned to room #{index}!"
+    index += 1
+  end
+  room_list
+end
+
+def printer(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
+  assign_rooms(names).each do |room|
+    puts room
+  end
+end
